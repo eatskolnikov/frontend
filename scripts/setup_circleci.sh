be00c58cfb08b5bb8bf839cde2873d22555a3cff
François Hodierne <francois@hodierne.net>
2018-06-05 22:15:57 +0200

    test(circleci): remove google-chrome dependency

--- scripts/setup_circleci.sh
+++ scripts/setup_circleci.sh
@@ -2,26 +2,6 @@
 # This script only runs on circleci, just before the e2e tests
 # first version cfr. https://discuss.circleci.com/t/add-ability-to-cache-apt-get-programs/598/6
 
-
-if [ "$NODE_ENV" = "circleci" ]; then
-  echo "Installing Google Chrome for E2E tests";
-else
-  exit;
-fi
-
-# Install Google Chrome
-if [ ! -d "~/cache" ]; then
-  mkdir ~/cache
-fi
-cd ~/cache
-
-if [ ! -e "google-chrome-stable_current_amd64.deb" ]; then
-  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-fi;
-
-sudo dpkg -i ./google-chrome*.deb
-sudo apt-get install -f
-
 sudo apt-get install GraphicsMagick
 
 API_TARBALL_URL="https://codeload.github.com/opencollective/opencollective-api/tar.gz/";
